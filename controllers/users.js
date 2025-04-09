ba7c756d7ea634e91c20d91bfff013cb42b6aaad
annams <annaextrasteckel@gmail.com>
2025-04-09 16:19:37 -0700

    unprotected route complete to get user details

--- controllers/users.js
+++ controllers/users.js
@@ -2,10 +2,10 @@
 
 const express = require('express');
 const router = express.Router();
-
-const User = require('../models/user');
+const User = require('../models/user.js');
 const verifyToken = require('../middleware/verify-token');
 
+// route to get details of all users
 router.get('/', verifyToken, async (req, res) => {
     try {
         // Get a list of all users, but only return their username and _id
@@ -15,6 +15,20 @@ router.get('/', verifyToken, async (req, res) => {
     } catch (err) {
         res.status(500).json({ err: err.message });
     }
+});
+
+// route to get details of a user
+router.get('/:userId', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.userId);
+        if (!user) {
+            return res.status(400).json({ error: 'User not found.' });
+        };
+
+        res.json({ user });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    };
 });
 
 module.exports = router;
