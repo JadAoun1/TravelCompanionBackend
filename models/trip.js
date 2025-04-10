e624b29bd8ad7df4d6757f854424a0912d3375a3
annams <annaextrasteckel@gmail.com>
2025-04-10 16:15:53 -0700

    trip model updated to allow for multiple user roles

--- models/trip.js
+++ models/trip.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 
 const tripSchema = new mongoose.Schema(
   {
+    // Changing "name" to "title."
     name: {
       type: String,
       required: true,
@@ -15,7 +16,23 @@ const tripSchema = new mongoose.Schema(
     destination: { type: mongoose.Schema.Types.ObjectId, ref: "Destination" },
     // Reference userSchema in user.js
     // Converting travellers to an array to allow multiple users to be added to a trip
-    travellers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    // travellers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    // Converting travellers above to a new structure to allow for user roles. The goal is to allow multiple users to be assigned to one trip, and users will each have an assigned role.
+    travellers: [
+        {
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User",
+            },
+            role: {
+                type: String,
+                // Viewer, in theory, if the trip is made public (verus private) and does not have permissions to make any edits.
+                enum: ['Owner', 'Editor', 'Viewer'],
+                // Default ensures that the user doesn't have permissions to make any edits.
+                default: ['Viewer'],
+            }
+        }
+    ]
   },
 
   { timestamps: true }
