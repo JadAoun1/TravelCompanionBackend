a0449f83f3ac4da6f6e2179bd018971c5912b373
annams <annaextrasteckel@gmail.com>
2025-04-12 12:35:16 -0700

    get route to return place_id object

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,7 @@
 // controllers/places.js
 
+// An API proxy is a server that sits between an application and a backend API, acting as a go-between for requests and responses.
+
 const express = require('express');
 const router = express.Router();
 // Requiring axios as a way to make an HTTP request to an external API (versus interacting with my own database in MongoDB via Mongoose)
@@ -29,5 +31,26 @@ router.get('/autocomplete', verifyToken, async (req, res) => {
         res.status(500).json({ message: 'Error fetching place suggestions.' });
     };
 });
+
+// Get more details from the API about a specific place based on the place_id
+router.get('/:placeId', verifyToken, async (req, res) => {
+    try {
+        const { placeId } = req.params;
+        const response = await axios.get(`${PLACES_API_BASE}/details/json`, {
+            params: {
+                place_id: placeId,
+                // Fields to return: name (name), geometry (lng, lat), formatted_address (readable address)
+                fields: 'name,geometry,formatted_address',
+                key: GOOGLE_API_KEY,
+            }
+        });
+
+        res.json(response.data);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error fetching place details.' });
+    };
+});
+
 
 module.exports = router;
